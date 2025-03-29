0edc615b52f82e7fa36214e72aab41d280933963
yesiamkriti <kritijulia@gmai.com>
2025-03-29 19:04:41 +0530

    start with the frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <header>Zynetic EV </header>
+      <main>
+        
+      </main>
+      <footer>
+        <p>Copyright © 2023 Zynetic</p>
+        <p>All rights reserved</p>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+        <p>Contact Us</p>
+        <p>Follow us on social media</p>
+      </footer>
+     </div>
+    </>
+  )
+}
+
+export default App
