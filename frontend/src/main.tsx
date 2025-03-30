4ac310784aad79125f7232e81ca9f7d6281907ae
yesiamkriti <kritijulia@gmai.com>
2025-03-31 03:03:08 +0530

    adding cart and profile page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,10 +10,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.tsx'
 import Homepage from './pages/Homepage.tsx'
-import ProductPage from './pages/Productpage.tsx'
+import ProductPage from './pages/ProductPage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
+import { StoreProvider } from 'Store.tsx'
 
 const queryClient = new QueryClient();
 
@@ -27,9 +27,11 @@ const router = createBrowserRouter(
 )
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StoreProvider>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    </StoreProvider>
   </StrictMode>
 )
